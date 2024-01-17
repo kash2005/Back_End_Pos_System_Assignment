bf8e8b47d4c9d80f6183e0b0cd4722e17b0fe91e
kash2005 <kashmifernando3@gmail.com>
2024-01-17 12:42:50 +0530

    add user

--- src/main/java/lk/ijse/back_end_assignment1/api/User.java
+++ src/main/java/lk/ijse/back_end_assignment1/api/User.java
@@ -1,10 +1,14 @@
 package lk.ijse.back_end_assignment1.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.ijse.back_end_assignment1.db.UserDB;
+import lk.ijse.back_end_assignment1.dto.UserDTO;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -30,11 +34,54 @@ public class User extends HttpServlet {
     }
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+
+        if (action.equals("checkUser")){
+            String userName = req.getParameter("userName");
+            checkUser(req,resp,userName);
+        }
+    }
+
+    protected void checkUser(HttpServletRequest req, HttpServletResponse resp, String userName) {
+        UserDB userDB = new UserDB();
+        UserDTO user = userDB.getUser(userName, connection);
+
+        Jsonb jsonb = JsonbBuilder.create();
+
+        try {
+            var json = jsonb.toJson(user);
+            resp.setContentType("application/json");
+            resp.getWriter().write(json);
+        } catch (IOException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Do Post");
 
         if (req.getContentType() != null && req.getContentType().toLowerCase().startsWith("application/json")){
+            Jsonb jsonb = JsonbBuilder.create();
+
+            UserDTO userDTO = jsonb.fromJson(req.getReader(),UserDTO.class);
+
+            System.out.println(userDTO.getUserName());
+            var userDB = new UserDB();
+            boolean result = userDB.saveUser(connection,userDTO);
+
+            if (result){
+                resp.setStatus(HttpServletResponse.SC_OK);
+                resp.getWriter().write("User information saved successfully !");
+            }else {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"Failed to saved user information !");
+            }
 
         }
+
+
+
     }
 }
